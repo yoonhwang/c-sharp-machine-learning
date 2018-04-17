237338664767b20346349dcfb9a0112ea0204ed6
yoonhwang <35632832+yoonhwang@users.noreply.github.com>
2018-04-16 21:18:20 -0400

    Update Clustering.cs

--- ch.6/Clustering.cs
+++ ch.6/Clustering.cs
@@ -47,7 +47,11 @@ namespace Clustering
             );
 
             // Create a new K-Means algorithm with n clusters
+            Accord.Math.Random.Generator.Seed = 0;
+
             int[] numClusters = new int[] { 4, 5, 6, 7, 8 };
+            List<string> clusterNames = new List<string>();
+            List<double> silhouetteScores = new List<double>();
             for(int i = 0; i < numClusters.Length; i++)
             {
                 KMeans kmeans = new KMeans(numClusters[i]);
@@ -55,24 +59,96 @@ namespace Clustering
                 int[] labels = clusters.Decide(sampleSet);
 
                 string colname = String.Format("Cluster-{0}", numClusters[i]);
+                clusterNames.Add(colname);
+
                 normalizedDf.AddColumn(colname, labels);
                 ecommerceDF.AddColumn(colname, labels);
 
-                Console.WriteLine("\n\n* Centroids for {0} clusters:", numClusters[i]);
-                PrintCentroidsInfo(clusters.Centroids, features);
-                VisualizeClusters(normalizedDf, colname, "NetRevenuePercentile", "AvgUnitPricePercentile");
+                Console.WriteLine("\n\n\n#####################    {0}    ###########################", colname);
 
-                for(int j = 0; j < numClusters[i]; j++)
+                Console.WriteLine("\n\n* Centroids for {0} clusters:", numClusters[i]);
+
+                PrintCentroidsInfo(clusters.Centroids, features);
+                Console.WriteLine("\n");
+
+                VisualizeClusters(normalizedDf, colname, "NetRevenuePercentile", "AvgUnitPricePercentile");
+                VisualizeClusters(normalizedDf, colname, "AvgUnitPricePercentile", "AvgQuantityPercentile");
+                VisualizeClusters(normalizedDf, colname, "NetRevenuePercentile", "AvgQuantityPercentile");
+
+                for (int j = 0; j < numClusters[i]; j++)
                 {
                     GetTopNItemsPerCluster(ecommerceDF, j, colname);
                 }
 
-                break;
+                double silhouetteScore = CalculateSilhouetteScore(normalizedDf, features, numClusters[i], colname);
+                Console.WriteLine("\n\n* Silhouette Score: {0}", silhouetteScore.ToString("0.0000"));
+
+                silhouetteScores.Add(silhouetteScore);
+                Console.WriteLine("\n\n##############################################################\n\n\n");
             }
             
+            for(int i = 0; i < clusterNames.Count; i++)
+            {
+                Console.WriteLine("- Silhouette Score for {0}: {1}", clusterNames[i], silhouetteScores[i].ToString("0.0000"));
+            }
 
             Console.WriteLine("\n\n\nDONE!!");
             Console.ReadKey();
+        }
+
+        private static double CalculateSilhouetteScore(Frame<int, string> df, string[] features, int numCluster, string clusterColname)
+        {
+            double[][] data = BuildJaggedArray(df.Columns[features].ToArray2D<double>(), df.RowCount, features.Length);
+
+            double total = 0.0;
+            for(int i = 0; i < df.RowCount; i++)
+            {
+                double sameClusterAverageDistance = 0.0;
+                double differentClusterDistance = 1000000.0;
+
+                double[] point = df.Columns[features].GetRowAt<double>(i).Values.ToArray();
+                double cluster = df[clusterColname].GetAt(i);
+
+                for(int j = 0; j < numCluster; j++)
+                {
+                    double averageDistance = CalculateAverageDistance(df, features, clusterColname, j, point);
+
+                    if (cluster == j)
+                    {
+                        sameClusterAverageDistance = averageDistance;
+                    } else
+                    {
+                        differentClusterDistance = Math.Min(averageDistance, differentClusterDistance);
+                    }
+                }
+
+                total += (differentClusterDistance - sameClusterAverageDistance) / Math.Max(sameClusterAverageDistance, differentClusterDistance);
+            }
+
+            return total / df.RowCount;
+        }
+
+        private static double CalculateAverageDistance(Frame<int, string> df, string[] features, string clusterColname, int cluster, double[] point)
+        {
+            var clusterDF = df.Rows[
+                df[clusterColname].Where(x => (int)x.Value == cluster).Keys
+            ];
+            double[][] clusterData = BuildJaggedArray(
+                clusterDF.Columns[features].ToArray2D<double>(),
+                clusterDF.RowCount,
+                features.Length
+            );
+
+            double averageDistance = 0.0;
+            for (int i = 0; i < clusterData.Length; i++)
+            {
+                averageDistance += Math.Sqrt(
+                    point.Select((x, j) => Math.Pow(x - clusterData[i][j], 2)).Sum()
+                );
+            }
+            averageDistance /= (float)clusterData.Length;
+
+            return averageDistance;
         }
 
         private static void GetTopNItemsPerCluster(Frame<int, string> ecommerceDF, int cluster, string colname)
@@ -84,7 +160,7 @@ namespace Clustering
             .Select(x => (double)x.Value.KeyCount)
             .Sort().Reversed;
 
-            Console.WriteLine("\n* #{0} of {1} - Top 10:", cluster, colname);
+            Console.WriteLine("\n# {0} of {1} - Top 10:", cluster+1, colname);
             itemDistribution[itemDistribution.Keys.Take(10)].Print();
         }
 
